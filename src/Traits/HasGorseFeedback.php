c2143ac91435f26a048b14e660a1f55d7f9da645
Janyk <me@janyk.dev>
2025-03-09 13:18:37 +0100

    feat(HasGorseFeedback): add method for similar item neighbors

--- src/Traits/HasGorseFeedback.php
+++ src/Traits/HasGorseFeedback.php
@@ -5,6 +5,7 @@ namespace JanykSteenbeek\LaravelGorse\Traits;
 use DateTime;
 use JanykSteenbeek\LaravelGorse\Facades\Gorse;
 use JanykSteenbeek\LaravelGorse\Observers\GorseObserver;
+use Illuminate\Database\Eloquent\Collection;
 
 trait HasGorseFeedback
 {
@@ -74,5 +75,48 @@ trait HasGorseFeedback
     public function feedback(string $type, ?string $userId = null, ?DateTime $timestamp = null): int
     {
         return Gorse::insertFeedback($type, $userId, $this->gorseItemId(), $timestamp);
+    }
+
+    /**
+     * Get similar items (neighbors) for this item.
+     */
+    public function neighbors(int $number = 10): Collection
+    {
+        $recommendations = Gorse::getUserNeighbors($this->gorseItemId(), $number);
+
+        return $this->resolveRecommendations($recommendations);
+    }
+
+    /**
+     * Resolve recommendations into model instances with scores.
+     */
+    protected function resolveRecommendations(array $recommendations): Collection
+    {
+        return collect($recommendations)
+            ->mapToGroups(function ($recommendation) {
+                $modelClass = static::getModelFromGorseId($recommendation['Id']);
+                $id = static::getIdFromGorseId($recommendation['Id']);
+
+                return [$modelClass => [
+                    'id' => $id,
+                    'score' => $recommendation['Score'] ?? 0,
+                ]];
+            })
+            ->filter(fn ($items, $modelClass) => $modelClass !== null)
+            ->map(function ($items, $modelClass) {
+                $models = (new $modelClass)
+                    ->whereIn((new $modelClass)->getKeyName(), collect($items)->pluck('id'))
+                    ->get();
+
+                // Attach scores to the models
+                return $models->map(function ($model) use ($items) {
+                    $item = collect($items)->firstWhere('id', $model->getKey());
+                    $model->gorse_score = $item['score'];
+                    return $model;
+                });
+            })
+            ->reduce(function (Collection $carry, Collection $models) {
+                return $carry->merge($models);
+            }, new Collection);
     }
 }
